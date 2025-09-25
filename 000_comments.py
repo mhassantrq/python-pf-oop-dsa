e39a95ba53ac54176abb6c8c769c5dd24fe55587
mhassantrq <mhassantrq@gmail.com>
2025-09-25 21:00:01 +0500

    added comments.py

---
+++ 000_comments.py
@@ -0,0 +1,28 @@
+"""
+Comments are lines of codes that are not executed by the language translator, compiler, interpreter etc.
+
+These comments are used to explain codes, provide documentations or useful explanations.
+
+We'll use comments to explain different concepts in going forward.
+
+There are two types of comments in python. Basically theres just one type, the single line comment. But a way out using string
+declaration, we can write multi line comments as well.
+"""
+
+
+#   single line comments start with a hash symbol as at the start of this line
+
+
+
+"""
+Multi line comments are enclosed with three quotation marks as around this comment.
+
+However, this multiline comment can be assigned to a variable and used as well unlike single line comments.
+lets see below.
+"""
+
+
+
+var = """ this is multiline comment """
+
+print(var)
