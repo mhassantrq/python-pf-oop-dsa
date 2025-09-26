92849f5b33b122b786a5219cf48830f5767e0d41
mhassantrq <mhassantrq@gmail.com>
2025-09-26 10:51:00 +0500

    000 comments updated

--- pf/000_comments.py
+++ pf/000_comments.py
@@ -11,11 +11,17 @@ declaration, we can write multi line comments as well.
 
 
 #   single line comments start with a hash symbol as at the start of this line
-
+#   you can have multi line comments
+#   via using the single line comment method
+#   however, its repetitive writing the same hash symbol again and again
+#   hence, below is the method for multi line comment.
 
 
 """
 Multi line comments are enclosed with three quotation marks as around this comment.
+
+As stated above that python typically has single line comment, this multi line comment is basically a string value without
+assigning it to a variable.
 
 However, this multiline comment can be assigned to a variable and used as well unlike single line comments.
 lets see below.
@@ -26,3 +32,8 @@ lets see below.
 var = """ this is multiline comment """
 
 print(var)
+
+
+#   look at the type of this multi line comment
+#   this will print that the multi line comment is of class <str>, a string.
+print(type(var))
