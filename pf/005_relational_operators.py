c1e1debe4438fefbef79bd5b5749cf9260531b27
mhassantrq <mhassantrq@gmail.com>
2025-09-26 16:29:47 +0500

    added pf- relational operators

---
+++ pf/005_relational_operators.py
@@ -0,0 +1,51 @@
+"""
+
+Relational operators in Python
+
+There are total six relational operators which include:
+
+    >       greater than
+    <       less than
+    >=      greater than or equal to
+    <=      less than or equal to
+    ==      equal to
+    !=      not equal to
+
+these operators require two operands, 
+    left side operator right side
+and it returns either true or false.
+
+"""
+
+
+num1, num2 = 74, 60
+
+#   greater than operator
+res = num1 > num2
+print(f'Greater than Operator: {res}')
+
+
+#   less than operator
+res = num1 < num2
+print(f'Less than Operator: {res}')
+
+
+#   greater than or equal to operator
+res = num1 >= num2
+print(f'Greater than or equal to Operator: {res}')
+
+
+#   less than or equal to operator
+res = num1 <= num2
+print(f'Less than or equal to Operator: {res}')
+
+
+#   equal to operator
+res = num1 == num2
+print(f'Equal to Operator: {res}')
+
+
+#   not equal to operator
+res = num1 != num2
+print(f'Not equal to Operator: {res}')
+
