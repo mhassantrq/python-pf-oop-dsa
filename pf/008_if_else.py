26d792909fd1d6a7b8b6c971695d314943e31e9b
mhassantrq <mhassantrq@gmail.com>
2025-09-27 23:27:42 +0500

    if else - not yet completed

--- pf/008_if_else.py
+++ pf/008_if_else.py
@@ -3,5 +3,22 @@ Conditional Structure: If else
 
 These are used to control the flow of the code execution.
 
+lets see basic example of how this works below
 """
 
+num1, num2 = 5, 6
+
+if num1 < num2:
+    print('Num1 is greater')
+    #   any thing written under if statement with indentation is part of the body of if statement
+else:
+    print('Num2 is greater')
+
+"""
+Above is a simple example of use of if and else.
+
+unlike some other progamming languages, python does not use curly brackets for declaring body of if and else.
+python uses indentation for this purpose. as can be seen in above example of if.
+
+if can work alone without else as well.
+"""
