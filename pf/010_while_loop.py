31d0d8633c80cc100438111605d242f746925728
mhassantrq <mhassantrq@gmail.com>
2025-09-28 21:16:24 +0500

    while loop remaining

---
+++ pf/010_while_loop.py
@@ -0,0 +1,12 @@
+"""
+Similar to for loop, while loop is used to iterate and run the same piece of code multiple times.
+
+while loop is run until a specific condition is not false.
+"""
+
+
+i = 0
+
+while i<= 10:
+    print(i)
+    i += 1
