f60496e47972290d1e7f4d6a7753f5fbc50a47ba
Hassan <mhassantrq@gmail.com>
2025-09-29 23:22:45 +0500

    Update 010_while_loop.py

--- pf/010_while_loop.py
+++ pf/010_while_loop.py
@@ -5,8 +5,13 @@ while loop is run until a specific condition is not false.
 """
 
 
-i = 0
+i = 0 # initial value of i set to 0
 
-while i<= 10:
+while i<= 10: # loop runs while i is less than or equal to 10
     print(i)
-    i += 1
+    i += 1 # increment value of i with +1 in every iteration
+
+"""
+the above loop will run until i reaches 10
+i starts at value 0. with every iteration, i is incremented by +1. so loop will run total 11 times. for i=0, i=1 and until i=10. 
+"""
