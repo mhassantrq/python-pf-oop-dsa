ffb6b6df0234c1e468d6f78779d67be764757758
mhassantrq <mhassantrq@gmail.com>
2025-10-13 19:56:31 +0500

    oop start

---
+++ oop/000.py
@@ -0,0 +1,8 @@
+"""
+Object oriented programming in python
+
+classes are blueprint of entities
+each class has properties and behaviour, variables and functions.
+
+class attributes are called using class object.
+"""
