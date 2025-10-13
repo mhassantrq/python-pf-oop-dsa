99de283aaff6658309844ddc1170d9f16f5c014f
mhassantrq <mhassantrq@gmail.com>
2025-10-13 21:04:57 +0500

    added type casting in pf

---
+++ pf/014_type_casting.py
@@ -0,0 +1,16 @@
+"""
+Type casting refers to changing the data type of a variable
+"""
+
+var_num_int = 2
+
+#   convert int to float by using float function around int value
+var_num_float = float(var_num_int)
+
+#   convert int to str by using str function around int value
+var_num_string = str(var_num_int)
+
+
+print(f'int variable: {var_num_int}, Type: {type(var_num_int)}')
+print(f'float variable: {var_num_float}, Type: {type(var_num_float)}')
+print(f'string variable: {var_num_string}, Type: {type(var_num_string)}')
