a9481ec10fd034480c8dfc78b1f9b2df4abfc857
mhassantrq <mhassantrq@gmail.com>
2025-10-14 19:45:10 +0500

    added linkedlist in ds-advanced

---
+++ ds/advanced/001_linked_list.py
@@ -0,0 +1,13 @@
+"""
+Linked List implementation in python
+"""
+
+class node():
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+    
+class LinkedList():
+    pass
+
+
